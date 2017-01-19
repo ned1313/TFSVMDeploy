151528628a8c47d3515b04e835842e883fbb523a
Ned Bellavance <ned.bellavance@gmail.com>
2017-01-19 10:51:29 -0500

    Add SAS Token and Container parameters.  Update Install scipt.

--- TFSVMDeploy/CustomScripts/InstallTFSScript.ps1
+++ TFSVMDeploy/CustomScripts/InstallTFSScript.ps1
@@ -4,6 +4,9 @@ param(
  [string] $FileContainerSASToken
 )
 
+Write-Output "File Container URL set to $FileContainerURL"
+Write-Output "File Containre SAS Token set to $FileContainerSASToken"
+
 #let's set some variables
 $installPath = "F:\Program Files\Microsoft Team Foundation Server 14.0"
 
@@ -14,6 +17,8 @@ $VSCisoFileName = "vs2015.3.com_enu.iso"
 $VSCAdminFileName = "AdminDeployment.xml"
 
 $dest = "F:\"
+
+Write-Output "Loading functions"
 
 #now a few functions
 function Disable-InternetExplorerESC {
@@ -44,9 +49,12 @@ function Copy-AzureBlob {
 		[string] $destPath
 	)
 	$URI = "$URL/" + $FileName + "?$SASToken"
-	Invoke-WebRequest -Uri $URI -Destination "$destPath\$FileName"
+	
+	Start-BitsTransfer -Source $URI -Destination "$destPath\$FileName"
 	return "$destPath\$FileName"
 }
+
+Write-Output "Preparing Disks"
 
 #Get the data disk prepared
 Get-Disk |
@@ -59,6 +67,7 @@ New-Partition -DriveLetter F -UseMaximumSize |
 
 Format-Volume -FileSystem NTFS -NewFileSystemLabel “TFSInstall” -Confirm:$false
 
+Write-Output "Create install path and disable ESC"
 mkdir $installPath
 
 #Turn off the pesky IE ESC, optionally UAC can be disabled too
@@ -67,11 +76,19 @@ Disable-InternetExplorerESC
 #Get the files to install TFS and VSC 2015
 $isoFile = Copy-AzureBlob -URL $FileContainerURL -SASToken $FileContainerSASToken -FileName $isoFileName -destPath $dest
 
+Write-Output "TFS ISO set to $isoFile"
+
 $VSCisoFile = Copy-AzureBlob -URL $FileContainerURL -SASToken $FileContainerSASToken -FileName $VSCisoFileName -destPath $dest
+
+Write-Output "VSC ISO set to $VSCisoFile"
 
 $VSCAdminFile = Copy-AzureBlob -URL $FileContainerURL -SASToken $FileContainerSASToken -FileName $VSCAdminFileName -destPath $dest
 
+Write-Output "VSC admin file set to $VSCAdminFile"
+
 #Mount the TFS ISO to get to the installers and retrieve the mount point
+Write-Output "Mounting TFS ISO to perform installation"
+
 Mount-DiskImage -ImagePath $isoFile -PassThru -ov mount
  
 $mount = ($mount | Get-Volume).DriveLetter + ":"
@@ -79,6 +96,7 @@ $mount = ($mount | Get-Volume).DriveLetter + ":"
 cd $mount
 
 #Run the TFS installer and wait until the process completes
+Write-Output "Running TFS Installer"
 .\Tfs2015.3.exe /Full /Quiet /CustomInstallPath $installPath
 
 do{Wait-Event -Timeout 5; $proc = Get-Process -Name "Tfs2015.3" -ErrorAction SilentlyContinue; Write-Output "TFS process still running"}while($proc)
@@ -86,20 +104,25 @@ do{Wait-Event -Timeout 5; $proc = Get-Process -Name "Tfs2015.3" -ErrorAction Sil
 #Run the basic unattended install for TFS
 cd "$installPath\Tools"
 
+Write-Output "Running TFS configuration"
 .\tfsconfig unattend /configure /type:basic
 
+Write-Output "Enable firewall rule"
 #Enable public access of TFS site
 Get-NetFirewallRule -DisplayName "Team Foundation Server:8080" | Set-NetFirewallRule -Profile Any
 
+Write-Output "Dismount TFS ISO"
 Dismount-DiskImage -ImagePath $isoFile
 
 #Mount the VS Community ISO to get to the installers and retrieve the mount point
+Write-Output "Mounting VSC ISO to perform installation"
 Mount-DiskImage -ImagePath $VSCisoFile -PassThru -ov mount
  
 $mount = ($mount | Get-Volume).DriveLetter + ":"
 
 cd $mount
 
+Write-Output "Running VSC Installer"
 .\vs_community.exe /adminfile $VSCAdminFile /quiet /norestart
 
 do{Wait-Event -Timeout 5; $proc = Get-Process -Name "vs_community" -ErrorAction SilentlyContinue; Write-Output "VS 2015 process still running"}while($proc)
@@ -108,6 +131,9 @@ mkdir $dest\Agent
 
 cd "$installPath\Build\Agent"
 
-.\VsoAgent.exe /Configure /RunningAsService /ServerUrl:http://localhost:8080/tfs /WorkFolder:$dest\Agent\_work /StartMode:Automatic /Name:Agent-Default /PoolName:default /WindowsServiceLogonAccount:"NT AUTHORITY\LOCAL SERVICE"
+Write-Output "Running Build Agent installation"
 
+ .\VsoAgent.exe /Configure /RunningAsService /ServerUrl:http://localhost:8080/tfs /WorkFolder:$dest\Agent\_work /StartMode:Automatic /Name:Agent-Default /PoolName:default /WindowsServiceLogonAccount:"NT AUTHORITY\LOCAL SERVICE" /WindowsServiceLogonPassword:"password" /Force
+
+Write-Output "Dismount VSC ISO"
 Dismount-DiskImage -ImagePath $VSCisoFile
